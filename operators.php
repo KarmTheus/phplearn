2ee1779b9c3734e08a619afbdadc2640dc2fc338
KarmTheus <141743719+KarmTheus@users.noreply.github.com>
2024-03-25 08:05:11 -0500

    Create operators.php

---
+++ operators.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        //VID 8
+        echo 2+6 ;
+
+        echo 10%4 ;
+
+        echo 6**2 ;
+    ?>
+
+    <?php
+        //VID 9
+        $x = 226;
+        $x+= 90;
+
+        echo $x;
+    ?>
+
+    <?php
+        //vid 10
+        $w = 4;
+        $y = 201;
+
+        if ($w < $y) {
+            echo "True";
+        }
+        else {
+            echo "False";
+        }
+    ?>
+
+    <?php
+        //vid 11
+        $q = 12;
+        echo ++$q;
+
+        echo --$q;
+    ?>
+
+    <?php
+        //vid 12
+        $x = 15;
+        $y = 39;
+
+        if ($x == $y xor 17== 17 ) {
+            echo "true";
+        }
+    ?>
+</body>
+</html>
