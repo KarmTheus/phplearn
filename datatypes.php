7de0cc8cb317a787c4ddff3d27d9517b0e29631c
KarmTheus <141743719+KarmTheus@users.noreply.github.com>
2024-04-03 08:44:13 -0500

    dcfvghnm

--- datatypes.php
+++ datatypes.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Data types</title>
 </head>
 <body>
+<?php
+    include 'includes/header.php';
+?>
     <?php
         //String
         $name = 'erm what "the" flip';
@@ -16,9 +22,9 @@
         //Float
         $name = 15.9739;
 
-        //Boolean
-        true = 1
-        false = 0
+        /* Boolean
+        true = 1;
+        false = 0; */
 
         //Array
         $names = array("Amanda","Jenny","Rico");
@@ -26,6 +32,6 @@
 
     ?>
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ includes/header.php
@@ -0,0 +1,35 @@
+
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="datatypes.php">Datatypes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="functions.php">Functions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="operators.php">Operators</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="conditionals.php">Conditionals</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="calculator.php">Calculator</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="calendar.php">Calendar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="loops.php">Loops</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
