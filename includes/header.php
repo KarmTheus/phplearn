7be2ef74dc7a1dbf16e145b63e0ff5cff3990281
KarmTheus <141743719+KarmTheus@users.noreply.github.com>
2024-04-04 08:38:11 -0500

    j

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,3 @@
-
-<body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">Home</a>
--- loops.php
+++ loops.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Document</title>
 </head>
-<body>
+
 <?php
     include 'includes/header.php';
 ?>    
