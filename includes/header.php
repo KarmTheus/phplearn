84297d481ff43c7f75bc3a9f5a1fc2a164bccd08
KarmTheus <141743719+KarmTheus@users.noreply.github.com>
2024-04-08 08:43:27 -0500

    z

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,9 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<?php
+    session_start();
+?>
+
+
+<nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +32,21 @@
         <li class="nav-item">
           <a class="nav-link" href="loops.php">Loops</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="superglobals.php">Superglobals</a>
+        </li>
       </ul>
     </div>
   </div>
 </nav>
+
+<?php
+  $_SESSION['username'] = "Youser1450";
+  echo $_SESSION['username'];
+
+  if (!isset($_SESSION['username'])) {
+    echo " You are not logged in <br>";
+  } else {
+    echo " You're Logged in! <br>";
+  }
+?>
--- superglobals.php
+++ superglobals.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,32 +6,17 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Data types</title>
+    <title>Cookies and Session</title>
 </head>
 <body>
 <?php
     include 'includes/header.php';
 ?>
     <?php
-        //String
-        $name = 'erm what "the" flip';
+        setcookie("name", "Markie", time()+ 172800);
 
-        //Integer
-        $name = 15;
 
-        //Float
-        $name = 15.9739;
-
-        /* Boolean
-        true = 1;
-        false = 0; */
-
-        //Array
-        $names = array("Amanda","Jenny","Rico");
-        echo $names['2']
 
     ?>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
